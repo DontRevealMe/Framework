777e7bd437dc6bea8c5fc049d30d6f48af715511
DontRevealMe <dontrevealmyidentity@gmail.com>
2020-06-15 21:06:56 -0400

    Documented Queue.lua

--- src/Shared/Modules/Queue.lua
+++ src/Shared/Modules/Queue.lua
@@ -7,11 +7,22 @@ local Signal = require("Signal")
 
 local Queue = {}
 Queue.__index = Queue
+Queue.ClassName = "Queue"
 
+--[[**
+    The function that will be called upon when the next item in queue is ready.
+    @param [t:Function] function
+    @returns [t:void]
+**--]]
 function Queue:SetUpdater(func)
     self.Updater = func
 end
 
+--[[**
+    Add an item to the queue.
+    @param [t:Variant] data
+    @returns [t:void]
+**--]]
 function Queue:Enqueue(data)
     table.insert(self.Queue, 1, data)
     if #self.Queue == 1 then 
@@ -19,14 +30,27 @@ function Queue:Enqueue(data)
     end
 end
 
+--[[**
+    Removes an item from the queue
+    @param [t:Number] index
+    @returns [t:void]
+**--]]
 function Queue:Dequeue(index)
     table.remove(self.Queue, index or #self.Queue)
 end
 
+--[[**
+    Wether or not the queue is running or not.
+    @returns [t:void]
+**--]]
 function Queue:IsSleeping()
     return coroutine.status(self._updateCoroutine) == "suspended" or coroutine.status(self._updateCoroutine) == "dead"
 end
 
+--[[**
+    Creates a new queue class
+    @returns [t:Queue]
+**--]]
 function Queue.new()
     local self = {}
     setmetatable(self, Queue)
