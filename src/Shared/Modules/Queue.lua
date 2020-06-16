2ddda230be987a5382f36e0d94e544237358f1f2
DontRevealMe <dontrevealmyidentity@gmail.com>
2020-06-15 20:27:45 -0400

    Update Queue.lua
     - Added Enqueue, Dequeue, and SetUpdater
     - Added coroutine updater loop

--- src/Shared/Modules/Queue.lua
+++ src/Shared/Modules/Queue.lua
@@ -8,29 +8,40 @@ local Signal = require("Signal")
 local Queue = {}
 Queue.__index = Queue
 
-function Queue:SetUpdater()
-
+function Queue:SetUpdater(func)
+    self.Updater = func
 end
 
 function Queue:Enqueue(data)
-
+    table.insert(self.Queue, 1, data)
 end
 
 function Queue:Dequeue(index)
-
+    table.remove(self.Queue, index or #self.Queue)
 end
 
 function Queue.new()
     local self = {}
     setmetatable(self, Queue)
     self._wakeUp = Signal.new()
-    self.Updater = coroutine.create(function(current)
+    self.Queue = {}
+    self._updateCoroutine = coroutine.create(function()
         while true do 
-
+            if #self.Queue == 0 or self["Updater"]==nil then 
+                coroutine.yield()
+            else
+                local current = self.Queue[#self.Queue]
+                local succ, err = pcall(self.Updater())
+                if not succ then
+                    warn(string.format("An unexpected error occoured at queue %"))
+                else
+                    table.remove(self.Queue)
+                end
+            end
         end
     end)
     self._wakeUpCon = self._wakeUp.Event:Connect(function()
-        
+        coroutine.resume(self.Updater)
     end)
     return self
 end
