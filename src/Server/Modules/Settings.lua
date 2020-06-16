29f37bd92c073f5b566db903f9015d341f13fc1d
DontRevealMe <dontrevealmyidentity@gmail.com>
2020-06-15 21:10:19 -0400

    Removed unncessary files

--- src/Server/Modules/Settings.lua
+++
@@ -1,5 +0,0 @@
-return {
-    DataStoreService = {
-        SaveInStudio = true; -- Save data in studio. Due to :BindToClose() being buggy, it can result to wait times of 30 seconds before studio even shuts down. 
-    }
-}
