46e87fdb208ada1dd519a06d797f04656fd99da2
DontRevealMe <dontrevealmyidentity@gmail.com>
2020-06-15 20:19:07 -0400

    Update Queue.lua

--- src/Shared/Modules/Queue.lua
+++ src/Shared/Modules/Queue.lua
@@ -2,15 +2,36 @@
     Name: Queue.lua
     Author: DontRevealMe
 --]]
+local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Framework"))
+local Signal = require("Signal")
 
 local Queue = {}
 Queue.__index = Queue
 
+function Queue:SetUpdater()
+
+end
+
+function Queue:Enqueue(data)
+
+end
+
+function Queue:Dequeue(index)
+
+end
 
 function Queue.new()
     local self = {}
     setmetatable(self, Queue)
+    self._wakeUp = Signal.new()
+    self.Updater = coroutine.create(function(current)
+        while true do 
 
+        end
+    end)
+    self._wakeUpCon = self._wakeUp.Event:Connect(function()
+        
+    end)
     return self
 end
 
