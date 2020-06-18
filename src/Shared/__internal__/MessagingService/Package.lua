edb07ef390b01a53e450896a90ef677da3d02e39
DontRevealMe <dontrevealmyidentity@gmail.com>
2020-06-17 20:39:15 -0400

    Added constructors and methods

--- src/Shared/__internal__/MessagingService/Package.lua
+++ src/Shared/__internal__/MessagingService/Package.lua
@@ -1,5 +1,6 @@
 --  Class that contains a group of packets
 --  @author DontRevealMe
+local Utility = require(script.Parent:WaitForChild("Util"))
 
 local Package = {}
 Package.__index = Package
@@ -12,8 +13,20 @@ function Package:AddPacket()
 
 end
 
-function Package.new(topic)
+function Package:GetSize()
+    local size = 0
+    for _,packet in pairs(self.Packets) do
+        size = size + Utility:GetSize(packet)
+    end
+end
 
+function Package.new(topic)
+    local self = {}
+    self.Topic = topic
+    self.Packets = {}
+    self.Size = Utility:GetSize(self.Packets)
+    Utility.CurrentlyBoxing[topic] = self
+    return self
 end
 
 return Package
