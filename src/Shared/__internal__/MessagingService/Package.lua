571aabda9f7a67089754932e50dd67c9b0e06d1f
DontRevealMe <dontrevealmyidentity@gmail.com>
2020-06-17 20:28:29 -0400

    Create Package.lua

---
+++ src/Shared/__internal__/MessagingService/Package.lua
@@ -0,0 +1,19 @@
+--  Class that contains a group of packets
+--  @author DontRevealMe
+
+local Package = {}
+Package.__index = Package
+
+function Package:Send()
+
+end
+
+function Package:AddPacket()
+
+end
+
+function Package.new(topic)
+
+end
+
+return Package
