9431357d91846238f482e652ec0a96f3e09eb16a
DontRevealMe <dontrevealmyidentity@gmail.com>
2020-06-18 14:35:06 -0400

    Added packet breaking up

--- src/Server/Modules/MessagingService/init.lua
+++ src/Server/Modules/MessagingService/init.lua
@@ -6,6 +6,7 @@ local HttpService = game:GetService("HttpService")
 local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Framework"))
 local Utility = require(script:WaitForChild("Util"))
 local Package = require(script:WaitForChild("Package"))
+local Packet = require(script:WaitForChild("Packet"))
 
 local module = {}
 
@@ -15,9 +16,24 @@ Utility.PacketQueue:SetUpdater(false, function()
     while #buffer > 0 do
         local packet = buffer[1]
         local package = Utility.CurrentlyBoxing[packet.Topic] or Package.new(packet.Topic)
-        if not package:AddPacket(packet, true) then
+        if packet:GetSize()>=800 then
+            --  Break the packet up
+            local UID = HttpService:GenerateGUID(false)
+            for i=1, math.ceil(packet:GetSize() / 800) do
+                local segmentData = HttpService:JSONEncode(packet.Data):sub( (i - 1) * 800 + 1, i * 800 )
+                local newPacket = Packet.new(segmentData, packet.Topic)
+                newPacket.Data.UID = UID
+                if not package:AddPacket(newPacket, true) then
+                    local newPackage = Package.new(packet.Topic)
+                    newPackage:AddPacket(newPacket, false)
+                    newPackage:Send(true)
+                end
+            end
+        elseif not package:AddPacket(packet, true) then
+            --  Package is full, send package.
             package:Send()
         else
+            --  Successful, remove the buffer from the list
             table.remove(buffer, 1)
         end
     end
