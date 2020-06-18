d8df0b730fcb70dd9ba28fa0a13d6676955cd1dd
DontRevealMe <dontrevealmyidentity@gmail.com>
2020-06-17 22:59:58 -0400

    Added :Destroy and a response signal

--- src/Server/Modules/MessagingService/Packet.lua
+++ src/Server/Modules/MessagingService/Packet.lua
@@ -1,6 +1,8 @@
 --  Packet class
 --  @author DontRevealMe
 
+local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Framework"))
+local Signal = require("Signal")
 local Utility = require(script.Parent:WaitForChild("Util"))
 local Packet = {}
 Packet.__index = Packet
@@ -9,12 +11,18 @@ function Packet:GetSize()
     return Utility:GetSize(self.Data)
 end
 
+function Packet:Destroy()
+    self.Response:Destroy()
+    self = nil
+end
+
 function Packet.new(data, topic)
     local self = {}
     setmetatable(self, Packet)
     self.Topic = topic
     self.Data = data
     self.Size = self:GetSize()
+    self.Response = Signal.new()
     return self
 end
 
