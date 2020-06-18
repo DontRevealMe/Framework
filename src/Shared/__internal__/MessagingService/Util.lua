8953dfff6c2428d602044b27c640791d13261ef2
DontRevealMe <dontrevealmyidentity@gmail.com>
2020-06-17 20:21:08 -0400

    Add packetbuffer

--- src/Shared/__internal__/MessagingService/Util.lua
+++ src/Shared/__internal__/MessagingService/Util.lua
@@ -7,7 +7,8 @@ local Queue = require("Queue")
 
 local Util = {
     PacketQueue = Queue.new(),
-    PublishQueue = Queue.new()
+    PublishQueue = Queue.new(),
+    PacketBuffer = {}
 }
 
 function Util:GetSize(...)
