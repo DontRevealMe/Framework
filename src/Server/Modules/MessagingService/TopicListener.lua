81ec95c14691b73ca9be2b2103749d688be51be6
DontRevealMe <dontrevealmyidentity@gmail.com>
2020-06-18 23:12:42 -0400

    TopicListener now has a connect method and a destroy method

--- src/Server/Modules/MessagingService/TopicListener.lua
+++ src/Server/Modules/MessagingService/TopicListener.lua
@@ -5,44 +5,40 @@ local MessagingService = game:GetService("MessagingService")
 local HttpService = game:GetService("HttpService")
 local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Framework"))
 local Signal = require("Signal")
+local Maid = require("Maid")
 local Utility = require(script.Parent:WaitForChild("Util"))
 
 local TopicListener = {}
 TopicListener.__index = TopicListener
 TopicListener._cache = {}
 
-local function GetSegment(UID)
-    if not Utility.PacketSegments[UID] then
-        Utility.PacketSegments[UID] = {
-            Packets = {},
-            Max = nil
-        }
-    end
-    return Utility.PacketSegments[UID]
-end
 
 function TopicListener:_invokeAllCallback(isCompleted, ...)
-    for _,callback in pairs(self._callBackTable) do
+    for _,callback in pairs(self._listeners) do
         if (callback.completeOnly and isCompleted) or not callback.completeOnly then
             callback.callback(...)
         end
     end
 end
 
-function TopicListener.new(topic, getIncomplete, callbackFunc)
-    if TopicListener._cache[topic] then
-        --  Check cache
-        table.insert(TopicListener._cache[topic]._callBackTable, {
-            completeOnly = not getIncomplete, callback = callbackFunc
-        })
-        return TopicListener._cache[topic]
-    end
+function TopicListener:Destroy()
+    self._maid:DoCleaning()
+    self = nil
+end
+
+function TopicListener:Connect(getCompleteOnly, callbackFunc)
+    return self.OnPacketRecivedSignal.Event:Connect(function(completed, ...)
+        if (completed and getCompleteOnly) or not getCompleteOnly then
+            callbackFunc(...)
+        end
+    end)
+end
+
+function TopicListener.new(topic, getIncomplete)
     local self = {}
     setmetatable(self, TopicListener)
-    self.Signal = Signal.new()
-    self._callBackTable = {
-        {completeOnly = not getIncomplete, callback = callbackFunc}
-    }
+    self.OnPacketRecivedSignal = Signal.new()
+    self._maid = Maid.new()
     self.Connection = MessagingService:SubscribeAsync(topic, function(package)
         local timeSent = package.Sent
         package = HttpService:JSONDecode(package.Data)
@@ -59,14 +55,18 @@ function TopicListener.new(topic, getIncomplete, callbackFunc)
                     for _,pack in pairs(segment) do
                         buildPacket = buildPacket .. pack.Data
                     end
-                    self:_invokeAllCallback(true, HttpService:JSONDecode(buildPacket), timeSent)
+                    self.OnPacketRecivedSignal:Fire(true, HttpService:JSONDecode(buildPacket), timeSent)
                 end
             end
             if not packet["UID"] or getIncomplete then
-                self:_invokeAllCallback(not packet["UID"], packet.Data, timeSent)
+                self.OnPacketRecivedSignal:Fire(not packet["UID"], packet.Data, timeSent)
             end
         end
     end)
+
+    self._maid:GiveTask(self.Connection)
+    self._maid:GiveTask(self.Signal)
+
     return self
 end
 
