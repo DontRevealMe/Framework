f3796eb12a557c0701a574e80c846d335fcf9b1e
DontRevealMe <dontrevealmyidentity@gmail.com>
2020-06-20 21:57:41 -0400

    Added MessagingService settings

--- src/Server/Modules/Settings.lua
+++ src/Server/Modules/Settings.lua
@@ -5,5 +5,11 @@ return {
     MessagingService = {
         UseSubChannels = true; --    Have channels enabled on by default.
         TotalSubChannels = 3; --    Total channels that will be reserved for subchannels.
+        SizeLimits = {
+            DataSize = 800; --  Maximum size of inputted data.
+            PacketSize = 850; --  Maximum size of a packet. The difference between DataSize and PacketSize will generally be the max size for your name if you're using subchannels.
+            PackageSize = 950; --   Maximum size of a package. 
+        },
+        MaxPackageAttempts = 5; -- Maximum failed attempts for a package in queue to be dropped.
     }
 }
