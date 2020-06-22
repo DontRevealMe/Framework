4a79e99397b36a67a0a795e003412a958e6be842
DontRevealMe <dontrevealmyidentity@gmail.com>
2020-06-21 22:28:32 -0400

    Added methods to OrderedDataStore

--- src/Server/Modules/DataStoreService/DataStoreCalling/OrderedDataStore.lua
+++ src/Server/Modules/DataStoreService/DataStoreCalling/OrderedDataStore.lua
@@ -1,3 +1,4 @@
+--  OrderedDataStore calling method
 --  @author DontRevealMe
 
 local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Framework"))
@@ -9,7 +10,25 @@ OrderedDataStore.__index = OrderedDataStore
 
 function OrderedDataStore:SetAsync(key, value)
     return Promise.async(function(resolve)
-        return resolve(self.OrderedDataStore:SetAsync(key or "default", value))
+        return resolve(self.OrderedDataStore:SetAsync(key, value))
+    end)
+end
+
+function OrderedDataStore:UpdateAsync(key, transformFunction)
+    return Promise.async(function(resolve)
+        return resolve(self.OrderedDataStore:SetAsync(key, transformFunction))
+    end)
+end
+
+function OrderedDataStore:GetAsync(key)
+    return Promise.async(function(resolve)
+        return resolve(self.OrderedDataStore:GetAsync(key))
+    end)
+end
+
+function OrderedDataStore:RemoveAsync(key)
+    return Promise.async(function(resolve)
+        return resolve(self.OrderedDataStore:RemoveAsync(key))
     end)
 end
 
