78fa19e012a00f634c5ba659722dcfe80e5ed8be
DontRevealMe <dontrevealmyidentity@gmail.com>
2020-06-22 16:38:30 -0400

    Added basic methods and added metatable functions

--- src/Server/Modules/DataStoreService/init.lua
+++ src/Server/Modules/DataStoreService/init.lua
@@ -2,6 +2,7 @@
 --  @author DontRevealme
 
 local require = require(game:GetService("ReplicatedStorage"):WaitForChild("Framework"))
+local Configuration = require("Server.Settings").DataStoreService
 local Maid = require("Maid")
 local Signal = require("Signal")
 local Promise = require("Promise")
@@ -10,7 +11,18 @@ local MessagingService = require("MessagingService")
 
 local DataStoreService = {}
 DataStoreService._cache = {}
-DataStoreService.__index = DataStoreService
+DataStoreService.__index = function(tab, index)
+    index = (index=="Value" and "_value") or index
+    return tab[index]
+end
+DataStoreService.__newindex = function(tab, index, value)
+    index = (index=="Value" and "_value") or index
+    tab[index] = value
+    if index=="Value" then
+        tab._ValueChanged:Fire(value)
+    end
+end
+
 DataStoreService.ClassName = "DataStore"
 
 function DataStoreService:FlushAsync()
@@ -66,6 +78,22 @@ function DataStoreService:GetBackupAsync(backupNum)
     end)
 end
 
+function DataStoreService:BindToPlayer(player)
+
+end
+
+function DataStoreService:Destroy()
+
+end
+
+function DataStoreService:BindToClose(callback)
+
+end
+
+function DataStoreService:Increment(detla)
+
+end
+
 function DataStoreService.new(name, key, callingMethod)
     if DataStoreService._cache[name .. key] then return DataStoreService[name .. key] end
     assert(typeof(name)=="string", 
@@ -95,10 +123,20 @@ function DataStoreService.new(name, key, callingMethod)
     self.ClassName = callingMethod
     self.CallingMethod = require(script:WaitForChild("DataStoreCalling"):FindFirstChild(callingMethod)).new(name, key)
     self.Value = nil
-
+    self._onUpdate = Signal.new()
+    self._valueChanged = Signal.new()
+    self.ValueChanged = self._valueChanged.Event
+    self.OnUpdate = self._onUpdate.Event
+    self._maid = Maid.new()
+    
+    self._maid:GiveTask(self._onUpdate)
+    self._maid:GiveTask(self._valueChanged)
     DataStoreService._cache[name .. key] = self
 
     return self
 end
+
+-- MessagingService
+
 
 return DataStoreService
