d3bf111f22e5032b689562f6d96ab190420521a6
DontRevealMe <dontrevealmyidentity@gmail.com>
2020-06-22 17:04:28 -0400

    Update Settings.lua

--- src/Server/Modules/Settings.lua
+++ src/Server/Modules/Settings.lua
@@ -1,10 +1,13 @@
 return {
     DataStoreService = {
-        SaveInStudio = true; --    Save data in studio. Due to :BindToClose() being buggy, it can result to wait times of 30 seconds before studio even shuts down. 
+        SaveInStudio = true; --  Save data in studio. Due to :BindToClose() being buggy, it can result to wait times of 30 seconds before studio even shuts down.
+        OnUpdateMessaging = true; ---  If the Framework's OnUpdate is enabled.
     },
     MessagingService = {
-        UseSubChannels = false; --    Have channels enabled on by default.
-        TotalSubChannels = 3; --    Total channels that will be reserved for subchannels.
+        DefaultSubChannels = {
+            Enabled = false; --  If default subchannels should be on
+            Amount = 3; --  Amount of default subchannels
+        },
         SizeLimits = {
             DataSize = 800; --  Maximum size of inputted data.
             PacketSize = 850; --  Maximum size of a packet. The difference between DataSize and PacketSize will generally be the max size for your name if you're using subchannels.
